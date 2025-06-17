b74b43422deaa46c28c812beb88ad69165bb29c5
“Diana” <my_diana972212@gmail.com>
2025-06-17 14:39:12 +0200

    Lesson

--- lesson__3/2.py
+++ lesson__3/2.py
@@ -1,9 +1,5 @@
-a = 6
-b = 12
-c = a**b
-print (c)
-a = 6
-b = 1
-c = a**b
-print(c)
-hhnkll;
+a = int(input("Введите первое число: "))
+b = int(input("Введите первое число: "))
+result = a ** b
+print(f"{a} в степени {b} = {result}")
+
