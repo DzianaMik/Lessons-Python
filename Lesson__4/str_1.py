0a81d916e53ff7710ef8a0d08779ba5b34d79586
“Diana” <my_diana972212@gmail.com>
2025-06-18 12:15:22 +0200

    Lesson 4

--- Lesson__4/str_1.py
+++ Lesson__4/str_1.py
@@ -1,8 +1,8 @@
 phrase = input("Введите фразу: ")
 length = len(phrase)
 words = len(phrase.split())
-letters = "аеёиоуыэюя"
-count_letters = sum(map(letters.__contains, phrase))
+letters = "аеёиоуыэюяeyuo"
+count_letters = sum(map(letters.__contains__, phrase))
 print("Количество символов:", length)
 print("Количество слов:", words)
 print("Количество гласных:", count_letters)
