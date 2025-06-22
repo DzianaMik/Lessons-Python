1b5017d511b56c6d792609d35c642a853c2fa457
“Diana” <my_diana972212@gmail.com>
2025-06-22 16:05:20 +0200

    Lesson

--- lesson __5/lesson.py
+++ lesson __5/lesson.py
@@ -5,6 +5,8 @@ grades = {
     "Гена": 5,
     "Даша": 4
 }
-for student, grade in grades:
-    student, grade = grades.items()
+for student, grade in grades.items():
     print(f"{student} - {grade}")
+
+    average = sum(grades.values())/ len(grades.values())
+print(f"Средн оценка: {average}")
