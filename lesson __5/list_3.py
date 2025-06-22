1efc68749e773ce2bb6a25f5a0236e479e374808
“Diana” <my_diana972212@gmail.com>
2025-06-22 19:11:52 +0200

    Lesson

---
+++ lesson __5/list_3.py
@@ -0,0 +1,7 @@
+name = []
+name.append(input("Ввести имя: "))
+name.append(input("Ввести имя: "))
+name.append(input("Ввести имя: "))
+name.append(input("Ввести имя: "))
+name.append(input("Ввести имя: "))
+print(name[::-1])
