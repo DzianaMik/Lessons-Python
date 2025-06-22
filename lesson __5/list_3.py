fc2c277ac7a87924c5d79858694d94017a0f0c9c
“Diana” <my_diana972212@gmail.com>
2025-06-22 19:57:50 +0200

    Lesson

--- lesson __5/list_3.py
+++ lesson __5/list_3.py
@@ -4,4 +4,7 @@ name.append(input("Ввести имя: "))
 name.append(input("Ввести имя: "))
 name.append(input("Ввести имя: "))
 name.append(input("Ввести имя: "))
-print(name[::-1])
+name[0], name[-1] = name [-1], name [0]
+remove = name.pop(2)
+print("Список:", name)
+print("Удаленный элемент:", remove)
