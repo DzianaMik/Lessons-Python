c4efdcca634531f4f8506175da0a08ae64303ddc
“Diana” <my_diana972212@gmail.com>
2025-06-22 20:33:09 +0200

    Lesson

---
+++ lesson __5/list_6.py
@@ -0,0 +1,7 @@
+text = [1, 1, 2, 3, 21, 8, 13, 21, 34, 55, 89]
+minimum = min(text)
+maximum = max(text)
+summaries = sum(text)
+print(f"Минимальное значение: {minimum}")
+print(f"Максимальное значение: {maximum}")
+print(f"Сумма: {summaries}")
