240a1c18684cbe67a4d520465e0184b0a80c6426
“Diana” <my_diana972212@gmail.com>
2025-06-23 19:45:13 +0200

    Lesson

--- lesson __5/1.py
+++ lesson __5/1.py
@@ -1,16 +1,7 @@
 product1 = input("Ввести 1 товар и цена: ")
 product2 = input("Ввести 2 товар и цена: ")
 product3 = input("Ввести 3 товар и цена: ")
+name1, price1 = product1.split()
+name2, price2 = product2.split()
+name3, price3 = product3.split()
 products = {}
-for part in [product1, product2, product3]:
-    name, price = part.split()
-    products[name] = float(price)
-print("\nСписок товаров:")
-for name, price in products.items():
-    print(f"{name} - {price}")
-price = input("\nВведите название товара увеличенного на 15%: ")
-if price in products:
-   increased = products[price] * 1.15
-   print(f"Цена{price} увеличена на 15%: {increased}")
-total = sum(products.values())
-print(f"\nСумма всех товаров: {total}")
