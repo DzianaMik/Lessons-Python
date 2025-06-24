4a13328f5846294af6fe74263a43c55fd61d8cc8
“Diana” <my_diana972212@gmail.com>
2025-06-24 15:31:26 +0200

    Lesson

--- lesson__6/homework/3.py
+++ lesson__6/homework/3.py
@@ -1,3 +1,13 @@
 '''
 Запросить 3 числа. Вывести наибольшее  из них. Решить используя if.
 '''
+
+number1 = int(input("Ввести число: "))
+number2 = int(input("Ввести число: "))
+number3 = int(input("Ввести число: "))
+if number1 > number2 and number1 > number3:
+    print("Наибольшее", number1)
+elif number2 > number1 and number2 > number3:
+    print("Наибольшее", number2)
+else:
+    print("Наибольшее", number3)
