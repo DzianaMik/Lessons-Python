dcfa2f3e8c2aef33c0bae49ecac0927803a64ddd
“Diana” <my_diana972212@gmail.com>
2025-06-27 12:59:31 +0200

    Lesson

--- lesson__6/homework/pyk.py
+++ lesson__6/homework/pyk.py
@@ -1,9 +1,9 @@
-text1 = input("Ввести число: ")
-text2 = input("Ввести число: ")
+text1 = int(input("Ввести число: "))
+text2 = int(input("Ввести число: "))
 text3 = int(input("Ввести число: "))
-if text1 == "admin" and text2 == 123456:
-    print("Разрешено")
-elif text1 == "vasya" and text2 == "vas123" and text3 < 60:
-    print("Разрешено")
+if text1 > text2 and text1 > text3:
+    print("yes", text1)
+if text2 > text1 and text2 > text3:
+    print("no", text2)
 else:
-    print("Запрещено")
+    print("Yes2", text3)
