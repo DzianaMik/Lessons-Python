6dff6a61fd41e64ad8145f6422855fdae6ce0f90
“Diana” <my_diana972212@gmail.com>
2025-06-28 18:36:25 +0200

    Lesson

--- lesson__7/homework/1.py
+++ lesson__7/homework/1.py
@@ -2,4 +2,17 @@
 Запросить у учителя оценки ученика по одной до тех пор пока он не введет 0. 
 Выдать средний бал ученика.
 """
+total = 0
+count = 0
+while True:
+    grades = int(input("Ввести оценки: "))
+    if grades == 0:
+        break
+    total += grades
+    count += 1
+if count > 0:
+    average = total / count
+    print(f"Средний балл ученика: {average}")
+else:
+    print("Ошибка")
 
