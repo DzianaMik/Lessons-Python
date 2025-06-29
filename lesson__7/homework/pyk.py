874db3959247826f5ce13fc0a15807db34445010
“Diana” <my_diana972212@gmail.com>
2025-06-29 15:08:13 +0200

    Lesson

--- lesson__7/homework/pyk.py
+++ lesson__7/homework/pyk.py
@@ -1,15 +1,4 @@
-total = 0
-count = 0
-while True:
-    text = int(input("Oki"))
-    if text == 0:
-        break
-    total += text
-    count += 1
-if count > 0:
-    maximum = max(total)
-    print(f"Максимальное: {maximum}")
-else:
-    print("Ошибка")
+
+
 
   
