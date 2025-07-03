4fecdbf35cbab5397ad63a8c50353ed7a103cb75
“Diana” <my_diana972212@gmail.com>
2025-07-03 13:15:57 +0200

    Lesson

--- lesson8/homework/6.py
+++ lesson8/homework/6.py
@@ -7,4 +7,15 @@ Yes - если число уже встречалось и No, если нет
 если в списке не все целые числа вернуть False.
 
 """
-
+def yes_or_n(full_number):
+     if not all(isinstance(x, int) for x in full_number):
+        return False
+     seen = set()    
+     result = []
+     for num in full_number:
+        if num in seen:
+            result.append("Yes")
+        else:
+            result.append("No")
+            seen.add(num)
+     return result
