2a3f976cd2d583eb196991cd5fbf33cdf3ebd6ce
“Diana” <my_diana972212@gmail.com>
2025-07-06 15:40:59 +0200

    Lesson

--- lesson9/homework/6.py
+++ lesson9/homework/6.py
@@ -4,3 +4,9 @@
 Отсортировать словарь по температуре в порядке возрастания и обратно.
 
 """
+
+temps = {"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}
+sorted_asc = dict(sorted(temps.items(), key=lambda item: item[1]))
+sorted_desc = dict(sorted(temps.items(), key=lambda item: item[1], reverse=True))  #reverse=True — сортировка в обратном порядке
+print("По возрастанию:", sorted_asc)
+print("По убыванию:", sorted_desc)
--- lesson9/homework/pyk.py
+++ lesson9/homework/pyk.py
@@ -1,5 +1,1 @@
-count = 0
-def greet_user(text):
-    global count
-    count += 1
-    print(f"{count}: {text}")
+
