b4e4e24656989d7c70b2a8d70f591d821f3c3580
“Diana” <my_diana972212@gmail.com>
2025-07-06 16:28:44 +0200

    Lesson

--- lesson9/homework/pyk.py
+++ lesson9/homework/pyk.py
@@ -1,1 +1,5 @@
-
+items = [10, "apple", True, 5, "banana", False, 3.14, "cherry"]
+text1 = list(filter(lambda x: isinstance(x, (int, float)), items))
+text2 = list(filter(lambda x: isinstance(x, str), items))
+print(text1)
+print(text2)
