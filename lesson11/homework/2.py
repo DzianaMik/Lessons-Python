0c8a749de40be50fae28cb1705b50b7797624e9e
“Diana” <my_diana972212@gmail.com>
2025-07-27 12:10:33 +0200

    Lesson

--- lesson11/homework/2.py
+++ lesson11/homework/2.py
@@ -20,3 +20,45 @@
 
 Вывести студентов, у которых средний балл больше 8
 """
+
+class Student:
+    def __init__(self, surname, name, group, grades=None):
+        self.surname = surname
+        self.name = name
+        self.group = group
+        self.grades = grades if grades else []
+
+    def add_grade(self, *new_grades):
+        for grade in new_grades:
+            if 1 <= grade <= 10:
+                self.grades.append(grade)
+            else:
+                print(f"Оценка {grade} не допустима (только от 1 до 10)")
+
+    def average_grade(self):
+        if not self.grades:
+            return 0
+        return sum(self.grades) / len(self.grades)
+
+    def __eq__(self, other):
+        return self.average_grade() == other.average_grade()
+
+    def __ne__(self, other):
+        return self.average_grade() != other.average_grade()
+
+    def __lt__(self, other):
+        return self.average_grade() < other.average_grade()
+
+    def __gt__(self, other):
+        return self.average_grade() > other.average_grade()
+
+    def __le__(self, other):
+        return self.average_grade() <= other.average_grade()
+
+    def __ge__(self, other):
+        return self.average_grade() >= other.average_grade()
+
+    def __str__(self):
+        return f"{self.surname} {self.name}, группа {self.group}, средний балл: {self.average_grade():.2f}"
+
+
